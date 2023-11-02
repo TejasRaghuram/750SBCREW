6247293f65ba3b75e7990b7a6ca478ecf59a35bb
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-11-02 16:35:44 -0400

    Added Page Routing

---
+++ src/pages/Gallery.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Gallery() {
+    return (
+        <div>
+            <p>This is the Gallery Page</p>
+        </div>
+    );
+}
+
+export default Gallery;
---
+++ src/pages/Sponsor.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sponsor() {
+    return (
+        <div>
+            <p>This is the Sponsor Page</p>
+        </div>
+    );
+}
+
+export default Sponsor;

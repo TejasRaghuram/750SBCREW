623d839e02621b911220d55a3db0e92a61f506f0
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-11-13 22:48:11 -0500

    Created About Selector

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,31 @@
-import React from "react";
+import { React, useState } from "react";
+import "./../styles/About.css"
 
 function About() {
+    const [team, setTeam] = useState("S");
     return (
         <div>
-            <p>This is the About Page</p>
+            <h1 id="about_title">OUR TEAMS</h1>
+            <h1 id="about_teams">
+                <span class={ team === "S" ? "about_selected" : "about_selectable" } onClick={() => {
+                    setTeam("S");
+                }}>S</span>
+                <span class={ team === "B" ? "about_selected" : "about_selectable" } onClick={() => {
+                    setTeam("B");
+                }}>B</span>
+                <span class={ team === "C" ? "about_selected" : "about_selectable" } onClick={() => {
+                    setTeam("C");
+                }}>C</span>
+                <span class={ team === "R" ? "about_selected" : "about_selectable" } onClick={() => {
+                    setTeam("R");
+                }}>R</span>
+                <span class={ team === "E" ? "about_selected" : "about_selectable" } onClick={() => {
+                    setTeam("E");
+                }}>E</span>
+                <span class={ team === "W" ? "about_selected" : "about_selectable" } onClick={() => {
+                    setTeam("W");
+                }}>W</span>
+            </h1>
         </div>
     );
 }

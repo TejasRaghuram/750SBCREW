1e2850c946750383c79250398cccd9cbdd147521
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-11-15 21:27:19 -0500

    Created Hero Section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Logo from "./../images/logo.svg";
+import "./../styles/Home.css";
 
 function Home() {
     return (
         <div>
-            <p>This is the Home Page</p>
+            <div id="home_hero">
+                <img id="home_logo" src={ Logo } alt=""/>
+                <div id="home_title">
+                    <h1 class="home_title_text">SOUTH</h1>
+                    <h1 class="home_title_text">BRUNSWICK</h1>
+                    <h1 class="home_title_text">ROBOTICS</h1>
+                </div>
+            </div>
         </div>
     );
 }
